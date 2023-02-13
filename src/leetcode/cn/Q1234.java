ea849f2d85e9f6a18077a74d19b4f99306786c1e
Shen Tianqi <1055862487@qq.com>
2023-02-13 10:50:55 +0800

    2月13日每日一题
    特别注意和string相关的操作 可能非常耗时
    
    除此以外，idea不适合直接在源码中写一个超长字符串

---
+++ src/leetcode/cn/Q1234.java
@@ -0,0 +1,40 @@
+package leetcode.cn;
+
+import java.util.Arrays;
+
+public class Q1234 {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        System.out.println(s.balancedString("RRRE"));
+    }
+}
+
+class Solution {
+    public int balancedString(String s) {
+        int ret = s.length();
+        int[] infos = new int['Z'];
+        Arrays.fill(infos, 0);
+        int target = ret / 4;
+        char[] schars = s.toCharArray();
+        for (char ch : schars) {
+            infos[ch]++;
+        }
+        if (check(infos, target)) return 0;
+        int leftPos = 0, rightPos = 0;
+        while (rightPos < s.length()) {
+            infos[schars[rightPos]]--;
+            while (check(infos, target) && leftPos < s.length()) {
+                ret = Math.min(ret, rightPos - leftPos + 1);
+                infos[schars[leftPos]]++;
+                leftPos++;
+            }
+            rightPos++;
+        }
+        return ret;
+    }
+
+    public boolean check(int[] cnt, int partial) {
+        return cnt['Q'] <= partial && cnt['W'] <= partial && cnt['E'] <= partial && cnt['R'] <= partial;
+    }
+
+}
