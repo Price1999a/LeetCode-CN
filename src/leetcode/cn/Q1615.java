6ec2c1b4a86dd464c31da44cb0bbf6aefe858d5d
Shen Tianqi <1055862487@qq.com>
2023-03-15 20:19:16 +0800

    3月15日每日一题

---
+++ src/leetcode/cn/Q1615.java
@@ -0,0 +1,24 @@
+package leetcode.cn;
+
+public class Q1615 {
+}
+
+class Solution {
+    public int maximalNetworkRank(int n, int[][] roads) {
+        boolean[][] connected = new boolean[n][n];
+        int[] cnt = new int[n];
+        for (int[] road : roads) {
+            connected[road[0]][road[1]] = true;
+            connected[road[1]][road[0]] = true;
+            cnt[road[0]]++;
+            cnt[road[1]]++;
+        }
+        int ret = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                ret = Math.max(ret, cnt[i] + cnt[j] - (connected[i][j] ? 1 : 0));
+            }
+        }
+        return ret;
+    }
+}
