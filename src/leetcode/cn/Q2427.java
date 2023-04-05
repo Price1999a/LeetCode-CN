355e36380deab5f345f4669b0006d9a506ae7c52
Shen Tianqi <1055862487@qq.com>
2023-04-05 14:30:20 +0800

    4月5日每日一题

---
+++ src/leetcode/cn/Q2427.java
@@ -0,0 +1,19 @@
+package leetcode.cn;
+
+public class Q2427 {
+    public int commonFactors(int a, int b) {
+        int g = gcd(a, b);
+        int ans = 0;
+        for (int x = 1; x <= g; ++x) {
+            if (g % x == 0) {
+                ++ans;
+            }
+        }
+        return ans;
+    }
+
+    private int gcd(int a, int b) {
+        return b == 0 ? a : gcd(b, a % b);
+    }
+
+}
